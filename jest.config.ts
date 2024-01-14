bbbae4c4e665d06bb2b21a5761318ee15b1546b9
iago-abner <iagoabner.dev@gmail.com>
2024-01-13 23:15:21 -0300

    jest config

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import { pathsToModuleNameMapper, JestConfigWithTsJest } from "ts-jest";
+import { compilerOptions } from "./tsconfig.json";
+
+const jestConfig: JestConfigWithTsJest = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>"],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  testMatch: ["**/(__tests__|tests)/**/*.+(spec|test).[jt]s?(x)"],
+};
+
+export default jestConfig;
